f8cb606c3721c90967045b750bf5c267e1e8434b
Nick(Seungyun Kim) <sy5072@naver.com>
2022-06-16 17:07:28 +0900

    [feat] 기본 map 구현

---
+++ MapClone/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  MapClone
+//
+//  Created by Seungyun Kim on 2022/06/16.
+//
+
+import SwiftUI
+import MapKit
+
+struct ContentView: View {
+    
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.5666791, longitude: 126.9782914), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
+    
+    var body: some View {
+        Map(coordinateRegion: $region, showsUserLocation: true, userTrackingMode: .constant(.follow))
+            .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MapClone/MapCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapCloneApp.swift
+//  MapClone
+//
+//  Created by Seungyun Kim on 2022/06/16.
+//
+
+import SwiftUI
+
+@main
+struct MapCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
