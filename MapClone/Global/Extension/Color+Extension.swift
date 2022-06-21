7b0586651c24b08630d6cbcdc46ad60e0afab67d
Nick(Seungyun Kim) <sy5072@naver.com>
2022-06-21 16:44:39 +0900

    [feat] color asset & mainview 구현

---
+++ MapClone/Global/Extension/Color+Extension.swift
@@ -0,0 +1,54 @@
+//
+//  Color+Extension.swift
+//  MapClone
+//
+//  Created by Seungyun Kim on 2022/06/21.
+//
+
+import SwiftUI
+
+extension Color {
+    static var backgroundGrey: Color {
+        return Color(hex: "#FBFBFB")
+    }
+    
+    static var lightGrey: Color {
+        return Color(hex: "#AAAAAA")
+    }
+    
+
+    static var iconGrey: Color {
+        return Color(hex: "#8A8A8A")
+    }
+    
+    static var popupGrey: Color {
+        return Color(hex: "#8E8E8E")
+    }
+    
+    static var listGrey: Color {
+        return Color(hex: "#F2F2F7")
+    }
+        
+    static var chartGrey: Color {
+        return Color(hex: "#EFEFEF")
+    }
+}
+
+extension Color {
+    init(hex: String) {
+        var hexFormatted: String = hex.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).uppercased()
+        
+        if hexFormatted.hasPrefix("#") {
+            hexFormatted = String(hexFormatted.dropFirst())
+        }
+        
+        assert(hexFormatted.count == 6, "Invalid hex code used.")
+        var rgbValue: UInt64 = 0
+        Scanner(string: hexFormatted).scanHexInt64(&rgbValue)
+        
+        self.init(red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+                  green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+                  blue: CGFloat(rgbValue & 0x0000FF) / 255.0)
+    }
+}
+
--- MapClone/View/HomeView.swift
+++ MapClone/View/HomeView.swift
@@ -6,10 +6,65 @@
 //
 
 import SwiftUI
+import Vision
+
 
 struct HomeView: View {
+    
+    @State var offset: CGFloat = 0
+    @State var lastOffset: CGFloat = 0
+    @GestureState var gestureOffset: CGFloat = -120
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            MapView()
+            GeometryReader { proxy -> AnyView in
+                let height = proxy.frame(in: .global).height
+                
+                return AnyView (
+                    ZStack{
+                        Rectangle()
+                            .foregroundColor(.white)
+                            .clipShape(CustomCorner(corners: [.topLeft,.topRight], radius: 20))
+                        
+                        BottomSheetView()
+                            .padding(.top, 10)
+                    }
+                    .offset(y: height - 70)
+                    .offset(y: -offset > 0 ? (-offset <= (height - 70) ? offset : -(height - 70)) : 0)
+                    .gesture(DragGesture().updating($gestureOffset, body: { value, out, _ in
+                        out = value.translation.height
+                        onChange()
+                    })
+                                .onEnded({ value in
+                                    
+                                    let maxHeight = height
+                                    withAnimation {
+                                        
+                                        if -offset > 70 && -offset < maxHeight / 2 {
+                                            offset = -(maxHeight * 0.4)
+    //                                        naviHide = false
+                                        }
+                                        else if -offset > maxHeight / 2 {
+                                            offset = -maxHeight
+    //                                        naviHide = true
+                                        }
+                                        else {
+                                            offset = 0
+                                        }
+                                        lastOffset = offset
+                                    }
+                                })
+                            )
+                )
+            }
+        }
+    }
+    
+    func onChange(){
+        DispatchQueue.main.async {
+            offset = gestureOffset + lastOffset
+        }
     }
 }
 
