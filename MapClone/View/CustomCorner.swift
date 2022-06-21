a0918348da099aacf62e7b04a2b5453276d47f6c
Nick(Seungyun Kim) <sy5072@naver.com>
2022-06-21 14:32:59 +0900

    [feat] custom corner 생성

---
+++ MapClone/View/CustomCorner.swift
@@ -0,0 +1,20 @@
+//
+//  bottomSheet.swift
+//  MapClone
+//
+//  Created by Seungyun Kim on 2022/06/21.
+//
+
+import SwiftUI
+
+struct CustomCorner: Shape {
+    
+    var corners: UIRectCorner
+    var radius: CGFloat
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        
+        return Path(path.cgPath)
+    }
+}
