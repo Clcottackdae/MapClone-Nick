082405afc9a88c37841c2677c6c5361df9b7325c
Nick(Seungyun Kim) <sy5072@naver.com>
2022-06-21 15:26:03 +0900

    [feat] myguide view 구현

---
+++ MapClone/View/MyGuideView.swift
@@ -0,0 +1,36 @@
+//
+//  MyGuideView.swift
+//  MapClone
+//
+//  Created by Seungyun Kim on 2022/06/21.
+//
+
+import SwiftUI
+
+struct MyGuideView: View {
+    var body: some View {
+        VStack (alignment: .leading) {
+            Text("나의 가이드")
+                .foregroundColor(.gray)
+            HStack {
+                Image(systemName: "plus")
+                    .foregroundColor(.blue)
+                    .frame(width: 45, height: 45)
+                    .background(RoundedRectangle(cornerRadius: 5)
+                        .foregroundColor(.gray))
+                Text("새로운 가이드")
+                Spacer()
+            }
+            .padding(15)
+            .background(RoundedRectangle(cornerRadius: 7)
+                .foregroundColor(.white))
+        }
+        .padding(15)
+    }
+}
+
+struct MyGuideView_Previews: PreviewProvider {
+    static var previews: some View {
+        MyGuideView()
+    }
+}
